9dcc507a1f2eccc3f3c2008f8c6b195a6d21050f
Alex Morrison <agmorrison@gmail.com>
2013-11-07 14:47:26 -0400

    just a simple table right now #2

--- flyer/views.py
+++ flyer/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from flyer.models import Sale
+
+def index(request):
+    # latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([p.question for p in latest_poll_list])
+    # return HttpResponse(output)
+
+    all_sales = Sale.objects.order_by('-price_sale')[:5]
+    template = loader.get_template('index.html')
+    context = RequestContext(request, {
+        'all_sales': all_sales,
+    })
+    return HttpResponse(template.render(context))
